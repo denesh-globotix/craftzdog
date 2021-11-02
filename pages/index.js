40b65c58be9997dea0b5a34cf161181031dac2f3
globotix <denesh.siva@globotix.sg>
2021-11-03 01:17:26 +0800

    Added profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading} from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue} from '@chakra-ui/react'
 
 const Page = () => {
  return (
   <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} aligh="center">
+      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} aligh="center">
         Hello, I&apos;m a full stack developer
       </Box>
 
@@ -13,6 +13,10 @@ const Page = () => {
             den
           </Heading>
           <p>Digital creator</p>
+        </Box>   
+        <Box flexShrink={0} mt={{base:4, md:0}} ml={{md: 6}} textAlign="center">
+          <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" src="/images/takuya.jpg" alt="Profile Image"
+          /> 
         </Box>
       </Box>
     </Container>
